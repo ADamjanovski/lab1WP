bb0d28791667f75ffd0f0e184c16c95f594c1246
ADamjanovski <79108006+ADamjanovski@users.noreply.github.com>
2024-10-30 18:16:11 +0100

    Added new feature

---
+++ lab1WP/src/main/java/mk/finki/ukim/mk/lab/web/servlet/EventBookingServlet.java
@@ -0,0 +1,53 @@
+package mk.finki.ukim.mk.lab.web.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mk.finki.ukim.mk.lab.model.EventBooking;
+import mk.finki.ukim.mk.lab.service.EventBookingService;
+import mk.finki.ukim.mk.lab.service.EventService;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.web.IWebExchange;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name="EventBookingServlet" ,urlPatterns = "/eventBooking")
+public class EventBookingServlet extends HttpServlet {
+    private final EventBookingService eventBookingService;
+    private final SpringTemplateEngine springTemplateEngine;
+
+    public EventBookingServlet(EventBookingService eventService, SpringTemplateEngine springTemplateEngine) {
+        this.eventBookingService = eventService;
+        this.springTemplateEngine = springTemplateEngine;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        IWebExchange webExchange = JakartaServletWebApplication
+                .buildApplication(getServletContext())
+                .buildExchange(req, resp);
+
+        WebContext context = new WebContext(webExchange);
+        context.setVariable("ipAddress", req.getRemoteAddr());
+        String queryString = req.getQueryString();
+        List<String> values = new ArrayList<>();
+        if (queryString != null && !queryString.isEmpty()) {
+
+            EventBooking booking=eventBookingService.placeBooking(req.getParameter("name"),req.getParameter("attendeeName"),req.getRemoteAddr(),Integer.parseInt(req.getParameter("numOfTickets")));
+            context.setVariable("booking",booking);
+            springTemplateEngine.process("bookingConfirmation.html", context, resp.getWriter());
+
+        } else {
+            System.out.println("No query parameters found.");
+        }
+    }
+}
